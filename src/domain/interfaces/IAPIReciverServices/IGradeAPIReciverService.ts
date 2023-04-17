5b81fc731b01a570831ebaa3fcebf39650d642bb
Raczkowski,Michał M.W <m.raczkowski@student.fontys.nl>
2023-04-17 06:13:06 +0200

    intrtface grade api reciver

--- src/domain/interfaces/IAPIReciverServices/IGradeAPIReciverService.ts
+++ src/domain/interfaces/IAPIReciverServices/IGradeAPIReciverService.ts
@@ -1,4 +1,5 @@
+import GradeDTO from "../../../infrastructure/dto/GradeDTO";
 
 export interface IGradeAPIReciverService {
-
+    GetStudnetGrade(idCourse: number, idAssignment: number, studentCanvasId: number): Promise<GradeDTO>
 }
