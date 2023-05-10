72d0527f2cb886c077ce6ebe1ecb8bb80a6238f7
Grzegorz Malisz <g.malisz@student.fontys.nl>
2023-05-10 20:01:23 +0200

    Added retriving more assigment per request

--- src/infrastructure/recivers/AssignmentAPIReciverService.ts
+++ src/infrastructure/recivers/AssignmentAPIReciverService.ts
@@ -4,7 +4,9 @@ import AssignmentDTO from "../dto/AssignmentDTO";
 import IAssignmentAPIReciver from "../../domain/interfaces/IAPIReciverServices/IAssignmentAPIReciver";
 
 export default class AssignmentAPIReciverService extends BaseCanvasAPIReciverService implements IAssignmentAPIReciver {
-  apiRouteAll = (studentCanvasId: number, courseId: number) => `/api/v1/users/${studentCanvasId.toString()}/courses/${courseId.toString()}/assignments`;
+  // https://canvas.instructure.com/doc/api/file.pagination.html
+  // Added ?per_page=60 to get all of the assignments, this should be fixed in the final version with pagination
+  apiRouteAll = (studentCanvasId: number, courseId: number) => `/api/v1/users/${studentCanvasId.toString()}/courses/${courseId.toString()}/assignments?per_page=30`;
   apiRouteById = (courseId: number, assignmentId: number) => `/api/v1/courses/${courseId.toString()}/assignments/${assignmentId.toString()}`;
 
   constructor(token: string) {
